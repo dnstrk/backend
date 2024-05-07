d6266cd06a052d86e5b811030afaadc773d648d6
dnstrk <stfuidkama@gmail.com>
2024-05-07 17:53:13 +0300

    comm

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const { error } = require("console");
 const express = require("express");
-const fs = require('fs')
-const bodyParser = require('body-parser');
-const cors = require('cors')
+const fs = require("fs");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-const user = require('./user.json')
+const user = require("./user.json");
 
 const app = express();
 
-
-app.use(cors())
+app.use(cors());
 
 app.use(bodyParser.json());
 
@@ -17,84 +16,87 @@ app.listen(5000, () => {
     console.log(`Server start at 5000`);
 });
 
-
-app.post('/writeFile', (req, res)=>{
-    const data = req.body
-    if(Object.keys(data)=="winNum") {
+app.post("/writeFile", (req, res) => {
+    const data = req.body;
+    if (Object.keys(data) == "winNum") {
         //вычленение номера
-        const newNum = data.winNum
+        const newNum = data.winNum;
         //чтение текущих данных
-        const winContacts = JSON.parse(fs.readFileSync('contacts.json'))
+        const winContacts = JSON.parse(fs.readFileSync("contacts.json"));
         //перезапись номера
-        winContacts["win-staff"].winNum = newNum
+        winContacts["win-staff"].winNum = newNum;
         //конвертирование в строку
-        const newData = JSON.stringify(winContacts)
+        const newData = JSON.stringify(winContacts);
         //запись в файл
-        fs.writeFile('contacts.json', newData, (err)=>{
-            if(err) {
-                console.log(err)
+        fs.writeFile("contacts.json", newData, (err) => {
+            if (err) {
+                console.log(err);
             }
-            console.log('success')
-        })
-    } else if (Object.keys(data)=="akdNum") {
-        console.log("Akdor")
-    } else if (Object.keys(data)=="stimulNum") {
-        console.log('Stimul')
+            console.log("success");
+        });
+    } else if (Object.keys(data) == "akdNum") {
+        console.log("Akdor");
+    } else if (Object.keys(data) == "stimulNum") {
+        console.log("Stimul");
     }
 
-    if(Object.keys(data)=="winMail") {
+    if (Object.keys(data) == "winMail") {
         //вычленение номера
-        const newMail = data.winMail
+        const newMail = data.winMail;
         //чтение текущих данных
-        const winContacts = JSON.parse(fs.readFileSync('contacts.json'))
+        const winContacts = JSON.parse(fs.readFileSync("contacts.json"));
         //перезапись номера
-        winContacts["win-staff"].winMail = newMail
+        winContacts["win-staff"].winMail = newMail;
         //конвертирование в строку
-        const newData = JSON.stringify(winContacts)
+        const newData = JSON.stringify(winContacts);
         //запись в файл
-        fs.writeFile('contacts.json', newData, (err)=>{
-            if(err) {
-                console.log(err)
+        fs.writeFile("contacts.json", newData, (err) => {
+            if (err) {
+                console.log(err);
             }
-            console.log('success')
-        })
-    } else if (Object.keys(data)=="akdMail") {
-        console.log("Akdor")
-    } else if (Object.keys(data)=="stimulMail") {
-        console.log('Stimul')
+            console.log("success");
+        });
+    } else if (Object.keys(data) == "akdMail") {
+        console.log("Akdor");
+    } else if (Object.keys(data) == "stimulMail") {
+        console.log("Stimul");
     }
 
+    res.send("ok");
+});
 
-
-    res.send('ok')
- })
-
-app.get('/readFile', (req, res)=>{
+app.get("/readFile", (req, res) => {
     // const num = fs.readFileSync('test.txt')
-    const contacts = JSON.parse(fs.readFileSync('contacts.json'))
+    const contacts = JSON.parse(fs.readFileSync("contacts.json"));
 
-    setTimeout(()=>{
+    setTimeout(() => {
         res.json({
             winNum: contacts["win-staff"].winNum,
-            winMail: contacts["win-staff"].winMail
-        })
-    },500)
+            winMail: contacts["win-staff"].winMail,
+        });
+    }, 500);
 
     // setTimeout(()=>{
     //     res.json({
     //         contacts
     //     })
     // },1000)
-})
+});
 
-app.post('/auth', (req, res)=>{
-    const data = req.body
-    if(data.login==user.admin.login&&data.password==user.admin.password) {
+app.post("/auth", (req, res) => {
+    const data = req.body;
+    if (
+        data.login == user.admin.login &&
+        data.password == user.admin.password
+    ) {
         res.json({
-            isAdmin: true
-        })
-        console.log('confirm')
+            isAdmin: true,
+        });
+        console.log("confirm");
     } else {
-        res.send('Пользователь не найден')
+        res.json({
+            isAdmin: false,
+            text: "Пользователь не найден",
+        });
     }
-})
+});
